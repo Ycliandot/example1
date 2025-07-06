ed15a504dc5afc7608d330fc18878f5e0c12e536
maxim.gorbachov <mxim.gorbachov@ideus.biz>
2025-07-06 19:11:47 +0300

    commit#16 Add foto for show employee

--- resources/views/admin/employees/show.blade.php
+++ resources/views/admin/employees/show.blade.php
@@ -3,6 +3,16 @@
 @section('content')
     <h1>Show employee</h1>
 
+    @if(Storage::disk('public')->exists($employee->photo))
+        <div class="container">
+            <div class="row w-50 h-50 mb-4">
+                <img src="{{ asset('storage/' . $employee->photo) }}" alt="photo">
+            </div>
+        </div>
+    @else
+        <div class="row">No photo</div>
+    @endif
+
     <table class="table">
         <tr>
             <td>Email</td>
