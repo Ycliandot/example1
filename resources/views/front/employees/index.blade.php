1c030d8de52fadd0c8e8e885e67e79555b5209e4
maxim.gorbachov <mxim.gorbachov@ideus.biz>
2025-07-06 17:29:55 +0300

    commit#12 Refactor front employee list

--- resources/views/front/employees/index.blade.php
+++ resources/views/front/employees/index.blade.php
@@ -13,9 +13,7 @@
             <th>Email</th>
             <th>First Name</th>
             <th>Last Name</th>
-            <th>Photo</th>
-{{--            <th></th>--}}
-{{--            <th></th>--}}
+            <th>Company</th>
         </tr>
         </thead>
         <tbody>
@@ -23,19 +21,10 @@
         @foreach($employees as $employee)
             <tr>
                 <td>{{ $employee->id }}</td>
-{{--                <th><a href="{{ route('post.show', $employee->id) }}">{{ $employee->title }}</a></th>--}}
                 <td>{{ $employee->email }}</td>
                 <td>{{ $employee->first_name}}</td>
                 <td>{{ $employee->last_name}}</td>
-                <td>{{ $employee->photo}}</td>
-{{--                <td><a href="{{ route('post.edit', $employee->id) }}" class="btn btn-secondary">Edit</a></td>--}}
-{{--                <td>--}}
-{{--                    <form action="{{ route('post.destroy', $employee->id) }}" method="post">--}}
-{{--                        @csrf--}}
-{{--                        @method('delete')--}}
-{{--                        <button class="btn btn-danger">Delete</button>--}}
-{{--                    </form>--}}
-{{--                </td>--}}
+                <td>{{ $employee->company->name}}</td>
             </tr>
         @endforeach
         </tbody>
